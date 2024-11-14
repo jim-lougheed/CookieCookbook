1ad4e1a1d0bbc827725da47620bdc42e547105d7
Jim <jims.codes@gmail.com>
2024-11-13 23:06:31 -0500

    Set up project flow and learn about dependency injection and dependency inversion

--- CookieCookbook/Program.cs
+++ CookieCookbook/Program.cs
@@ -1,2 +1,64 @@
-﻿var cookiesRecipeApp = new CookiesRecipesApp();
+﻿
+var cookiesRecipesApp = new CookiesRecipesApp(new RecipesRepository(), new RecipesConsoleUserInteraction());
 cookiesRecipesApp.Run();
+
+public class CookiesRecipesApp
+{
+    private readonly IRecipesRepository _recipesRepository;
+    private readonly IRecipesUserInteraction _recipesUserInteraction;
+    public CookiesRecipesApp(IRecipesRepository recipesRepository, IRecipesUserInteraction recipesUserInteraction)
+    {
+        _recipesRepository = recipesRepository;
+        _recipesUserInteraction = recipesUserInteraction;
+    }
+
+    public void Run()
+    {
+        var allRecipes = _recipesRepository.Read(filePath);
+        _recipesUserInteraction.PrintExistingRecipes(allRecipes);
+        _recipesUserInteraction.PromptToCreateRecipe();
+        var ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
+        if (ingredients.Count > 0)
+        {
+            var recipes = new Recipe(ingredients);
+            allRecipes.Add(recipes);
+            _recipesRepository.Write(filePath, allRecipes);
+            _recipesUserInteraction.ShowMessage("Recipe added: ");
+            _recipesUserInteraction.ShowMessage(recipes.ToString());
+        } else
+        {
+            _recipesUserInteraction.ShowMessage("No ingredients have been selected. " +)
+                "Recipe will not be saved.");
+        }
+        _recipesUserInteraction.Exit();
+    }
+}
+
+public interface IRecipesUserInteraction
+{
+    public void ShowMessage(string message);
+    public void Exit();
+}
+
+public class RecipesConsoleUserInteraction : IRecipesUserInteraction
+{
+    public void ShowMessage(string message)
+    {
+        Console.WriteLine(message);
+    }
+    public void Exit()
+    {
+        Console.WriteLine("Press any key to close.");
+        Console.ReadKey();
+    }
+
+}
+
+public interface IRecipesRepository
+{
+
+}
+
+public class RecipesRepository : IRecipesRepository
+{
+}
