940a4c6762b2b11fd490ac90ead27fedb39161e1
Jim <jims.codes@gmail.com>
2024-11-24 17:24:40 -0500

    Add global try catch block

--- CookieCookbook/Program.cs
+++ CookieCookbook/Program.cs
@@ -1,21 +1,30 @@
 ﻿
-using System.Text.Json;
 using CookieCookbook.App;
 using CookieCookbook.DataAccess;
 using CookieCookbook.FileStorage;
 using CookieCookbook.Recipes;
-using Microsoft.VisualBasic;
 using static CookieCookbook.Recipes.Recipe;
 
-const FileFormat format = FileFormat.Txt;
-IIngredientsRegister ingredientsRegister = new IngredientsRegister();
-IStringsRepository stringsRepository = format == FileFormat.Txt ? new StringsTextualRepository() : new StringsJSONRepository();
+try
+{
+    const FileFormat format = FileFormat.Txt;
+    IIngredientsRegister ingredientsRegister = new IngredientsRegister();
+    IStringsRepository stringsRepository = format == FileFormat.Txt ? new StringsTextualRepository() : new StringsJSONRepository();
 
-const string FileName = "recipes";
-var fileMetadata = new FileMetadata(FileName, format);
+    const string FileName = "recipes";
+    var fileMetadata = new FileMetadata(FileName, format);
 
-var cookiesRecipesApp = new CookiesRecipesApp(new RecipesRepository(stringsRepository, ingredientsRegister), new RecipesConsoleUserInteraction(ingredientsRegister));
-cookiesRecipesApp.Run(fileMetadata.ToPath());
+    var cookiesRecipesApp = new CookiesRecipesApp(new RecipesRepository(stringsRepository, ingredientsRegister), new RecipesConsoleUserInteraction(ingredientsRegister));
+    cookiesRecipesApp.Run(fileMetadata.ToPath());
+}
+catch (Exception exception)
+{
+    Console.WriteLine("Sorry! The application experienced an unexpected error and will have to be closed." +
+        "Error: " + exception.Message);
+    Console.WriteLine("Press any key to close.");
+    Console.ReadKey();
+}
+
 
 
 
